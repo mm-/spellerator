afc1428df505ddd3f6baf821f290d7582a2a7ae6
Masen Marshall <mm@masenmarshall.com>
2015-01-23 12:47:17 -0600

    Added more up to date dictionaries - shitty functions More up to date dictionaries solve a bunch of problems like single quotes. Functions just to make things more readable while I expand, silly now.

--- spellerator.sh
+++ spellerator.sh
@@ -2,34 +2,20 @@
 # Spell check a project 
 
 dir=$1
-library="$HOME/.spellerator"
+srhome="$HOME/.spellerator"
+library="$srhome/library"
+dicthome="$srhome/dicts"
 hsdefault="$HOME/.hunspell_en_US"
 # This var gets the last directory out of a path string, ie "/home/user/jank/github/myproject" becomes "myproject"
 project_name=$(echo $dir | awk 'BEGIN { FS = "/" } ; { print $NF }')
 
-# Make a backup of existing hunspell private dictionary if need be
-if [ -f $hsdefault ]
-    then
-    echo "Private dictionary for hunspell exists, backing it up to .bak"
-    mv $hsdefault $hsdefault.bak
-fi
 
 
-if [ -f $library/$project_name ]
-    then
-    echo "Using existing dictionary from library for project: $project_name"
-    cp $library/$project_name $hsdefault
-fi
+first_run(){
 
-
-# Main loop, run hunspell against every ASCII file in specified dir
-
-for file in $(find $dir -path $dir/.git -prune -o -type f -exec file -F "" {} + | grep 'ASCII\|UTF-8' | awk '{ print $1 }') 
-  do
-    echo $file
-    hunspell $file
-  done	
-
+    # TODO: Extend this to automatically place dictionary files from /dicts into $dicthome
+    
+    
 # Create library dir if needed
 if [ -d $library ]
     then
@@ -39,12 +25,54 @@ if [ -d $library ]
     mkdir $library
 fi
 
+}
+
+
+startup(){
+# Use existing personal dict if available
+if [ -f $library/$project_name ]
+    then
+    echo "Using existing dictionary from library for project: $project_name"
+    cp $library/$project_name $hsdefault
+fi
+
+# Make a backup of existing hunspell private dictionary if need be
+if [ -f $hsdefault ]
+    then
+    echo "Private dictionary for hunspell exists, backing it up to .bak"
+    mv $hsdefault $hsdefault.bak
+fi
+}
+
+check_files(){
+# Main loop, run hunspell against every ASCII file in specified dir
+
+for file in $(find $dir -path $dir/.git -prune -o -type f -exec file -F "" {} + | grep 'ASCII\|UTF-8' | awk '{ print $1 }') 
+  do
+    echo $file
+    hunspell $file
+  done	
+}
+
+
+end(){
 # Rename and move our file to library                                                                                                                                               
 echo "Saving your custom dictionary for later use"
 echo "It will be available in $library/$project_name"
 
 mv $hsdefault $library/$project_name
+}
 
+
+
+
+## Run ##
+
+#TODO: check for actual first run
+first_run
+startup
+check_files
+end
 
 
 
